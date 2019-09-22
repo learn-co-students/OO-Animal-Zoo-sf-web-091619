67d885832049963e42999927ce92ba8abf630206
Max Zakzook <mzakzook@me.com>
2019-09-22 09:42:53 -0700

    Done.

--- lib/Zoo.rb
+++ lib/Zoo.rb
@@ -1,3 +1,39 @@
 class Zoo
-
+    attr_accessor :name, :location
+    attr_reader :animals
+    @@all = []
+    def initialize(name, location)
+        @name = name
+        @location = location
+        @@all << self
+    end
+    def self.all
+        @@all
+    end
+    def animals
+        Animal.all.select do |animal|
+            animal.zoo == self
+        end
+    end
+    def animal_species
+        species = @animals.map do |animal|
+            animal.species
+        end
+        species.uniq
+    end
+    def find_by_species(species)
+        @animals.select do |animal|
+            animal.species == species
+        end
+    end
+    def animal_nicknames
+        @animals.map do |animal|
+            animal.nickname
+        end    
+    end
+    def self.find_by_location(location)
+        @@all.select do |zoo|
+            zoo.location == location
+        end    
+    end
 end
